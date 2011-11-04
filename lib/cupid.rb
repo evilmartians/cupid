dc73637a9fd0047e0d4bd6e155263f2240a0cdad
brainopia <brainopia@evilmartians.com>
2011-11-04 19:41:21 +0700

    Add Cupid#client

--- lib/cupid.rb
+++ lib/cupid.rb
@@ -1,11 +1,21 @@
+require 'savon'
 require 'cupid/version'
 
 class Cupid
-  attr_reader :username, :password, :account
+  attr_reader :client, :account
+  WSDL = 'https://webservice.s4.exacttarget.com/etframework.wsdl'
 
   def initialize(username, password, account)
-    @username = username
-    @password = password
-    @account  = account
+    @client  = get_client username, password
+    @account = account
+  end
+
+  private
+
+  def get_client(username, password)
+    Savon::Client.new(WSDL).tap do |client|
+      client.wsse.credentials username, password
+      client.wsse.timestamp = true
+    end
   end
 end
--- spec/cupid_spec.rb
+++ spec/cupid_spec.rb
@@ -3,8 +3,8 @@ require 'cupid'
 describe Cupid do
   subject { Cupid.new 'bob', 'x123', 42 }
 
-  it { should be }
-  its(:username) { should == 'bob' }
-  its(:password) { should == 'x123' }
+  its(:client) { should be }
+  its('client.wsse.username') { should == 'bob' }
+  its('client.wsse.password') { should == 'x123' }
   its(:account) { should == 42 }
 end
