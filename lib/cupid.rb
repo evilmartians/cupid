f41bbef95196215487f02a67a2bada01bdb6d927
brainopia <brainopia@evilmartians.com>
2011-11-06 15:09:27 +0700

    Set namespace and endpoint directly without fetching remote wsdl

--- lib/cupid.rb
+++ lib/cupid.rb
@@ -2,8 +2,10 @@ require 'savon'
 require 'cupid/version'
 
 class Cupid
+  ET_NAMESPACE = 'http://exacttarget.com/wsdl/partnerAPI'
+  ET_ENDPOINT  = 'https://webservice.s4.exacttarget.com/Service.asmx'
+
   attr_reader :client, :account
-  WSDL = 'https://webservice.s4.exacttarget.com/etframework.wsdl'
 
   def initialize(username, password, account)
     @client  = get_client username, password
@@ -13,7 +15,9 @@ class Cupid
   private
 
   def get_client(username, password)
-    Savon::Client.new(WSDL).tap do |client|
+    Savon::Client.new.tap do |client|
+      client.wsdl.namespace = ET_NAMESPACE
+      client.wsdl.endpoint  = ET_ENDPOINT
       client.wsse.credentials username, password
     end
   end
