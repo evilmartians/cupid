1de4e453febfed994402bb1d103db9a45681dce3
brainopia <brainopia@evilmartians.com>
2011-11-09 15:17:34 +0700

    Add retrieve_emails

--- lib/cupid/retrieve.rb
+++ lib/cupid/retrieve.rb
@@ -1,22 +1,32 @@
 class Cupid
   module Retrieve
+    EMAIL_FIELDS        = %w(ID Name)
     EMAIL_FOLDER_FIELDS = %w(ID Name ParentFolder.ID ParentFolder.Name)
 
     def retrieve(type, fields, options={})
       post :retrieve, :retrieve_request => {
         :object_type => type,
-        :properties => fields
+        :properties => fields,
+        'ClientIDs' => { 'ID' => account }
       }.merge(options)
     end
 
-    def retrieve_email_folders(fields=EMAIL_FOLDER_FIELDS)
-      retrieve 'DataFolder', fields, email_folder_filter
+    def retrieve_email_folders(fields = EMAIL_FOLDER_FIELDS)
+      retrieve 'DataFolder', fields, filter_email_folders
+    end
+
+    def retrieve_emails(name=nil, fields = EMAIL_FIELDS)
+      retrieve 'Email', fields, filter_email_like(name)
     end
 
     private
 
-    def email_folder_filter
+    def filter_email_folders
       filter 'ContentType', 'like', 'email'
+    end
+
+    def filter_email_like(name)
+      filter 'Name', 'like', name
     end
   end
 end
