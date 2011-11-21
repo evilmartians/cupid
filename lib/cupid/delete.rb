37e1fa212d0bbb2612cd24dcc6f62062bed01082
brainopia <brainopia@evilmartians.com>
2011-11-21 13:18:22 +0800

    Add delete_emails_like method

--- lib/cupid/delete.rb
+++ lib/cupid/delete.rb
@@ -3,5 +3,10 @@ class Cupid
     def delete_emails(ids)
       post :delete, server.emails(ids)
     end
+
+    def delete_emails_like(name)
+      ids = emails(name).map {|it| it[:id] }
+      delete_emails ids
+    end
   end
 end
--- spec/cupid/delete_spec.rb
+++ spec/cupid/delete_spec.rb
@@ -1,12 +1,21 @@
 describe Cupid::Delete do
   subject { Cupid::Test }
 
-  context '#delete_emails' do
-    let(:email) { subject.create_email 'subject', 'body' }
+  shared_examples_for 'deletion' do
+    let(:email) { subject.create_email email_name, 'body' }
     let(:email_id) { email[:new_id] }
-    let(:deletion) { subject.delete_emails [email_id] }
+    let(:email_name) { 'subject' }
 
     it { deletion.should be_success }
     it { subject.emails.each {|it| it.should_not include(:id => email_id) }}
+  end
+  context '#delete_emails' do
+    let(:deletion) { subject.delete_emails [email_id] }
+    it_should_behave_like 'deletion'
+  end
+
+  context '#delete_emails_like' do
+    let(:deletion) { subject.delete_emails_like email_name }
+    it_should_behave_like 'deletion'
   end
 end
